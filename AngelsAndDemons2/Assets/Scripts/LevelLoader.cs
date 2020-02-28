f646db978cb9789c323a384a41f6749758cee3da
Yunseo-Han <57244943+Yunseo-Han@users.noreply.github.com>
2020-02-28 04:49:52 -0800

    LevelLoader Script update

--- AngelsAndDemons2/Assets/Scripts/LevelLoader.cs
+++ AngelsAndDemons2/Assets/Scripts/LevelLoader.cs
@@ -1,14 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
+    [SerializeField] int timeToWait = 4;
+    int currentSceneIndex;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(WaitForTime());
+
+        }
+
     }
+
+    IEnumerator WaitForTime()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        loadNextScene();
+    }
+
+    public void loadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
 
     // Update is called once per frame
     void Update()
