3a281a100502c5b6b30a2f2c04b63e80dabb0708
Yunseo-Han <57244943+Yunseo-Han@users.noreply.github.com>
2020-02-29 02:20:24 -0800

    scene fades
    not completely fixed.

--- AngelsAndDemons2/Assets/Scripts/LevelLoader.cs
+++ AngelsAndDemons2/Assets/Scripts/LevelLoader.cs
@@ -7,22 +7,28 @@ public class LevelLoader : MonoBehaviour
 {
     [SerializeField] int timeToWait = 4;
     int currentSceneIndex;
+    public Animator transitionAnim;
 
     // Start is called before the first frame update
     void Start()
     {
+        transitionAnim = GetComponent<Animator>();
         currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         if (currentSceneIndex == 0)
         {
+
             StartCoroutine(WaitForTime());
-
         }
-
+        
     }
 
     IEnumerator WaitForTime()
     {
         yield return new WaitForSeconds(timeToWait);
+        Debug.Log("here");
+        transitionAnim.SetTrigger("end");
+        yield return new WaitForSeconds(1.5f);
+        Debug.Log("hi again");
         loadNextScene();
     }
 
