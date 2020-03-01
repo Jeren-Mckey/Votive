61ab491f4c5fd5a5239615dad9a28301bc35299c
Yunseo-Han <57244943+Yunseo-Han@users.noreply.github.com>
2020-03-01 01:06:09 -0800

    working scene fades
    Prefab: Level Loader
    Animator: Crossfade
    Animations: Crossfade_End, Crossfade_Start
    Script: LevelLoader

---
+++ AngelsAndDemons2/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float waitTime = 4f;
+    public float transitionTime = 1f;
+    public int nextLevelIndex = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadNextLevel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /* not using input for opening scene transitions
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            LoadNextLevel();
+        }
+        */
+    }
+
+    public void LoadNextLevel()
+    {
+        //StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+        StartCoroutine(LoadLevel(nextLevelIndex));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        yield return new WaitForSeconds(waitTime);
+        transition.SetTrigger("start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(levelIndex);
+    }
+}
