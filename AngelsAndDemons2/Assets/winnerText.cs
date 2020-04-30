d458d1a6680ca4a3cd2ed0f2a7f2f3f78c0fa51a
Aaron Jay Macaldo <Aaronjay.macaldo@sjsu.edu>
2020-04-29 18:53:16 -0700

    Added script to change winner message based on who won

---
+++ AngelsAndDemons2/Assets/winnerText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class winnerText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextMeshProUGUI winText = this.GetComponentInChildren<TextMeshProUGUI>();
+        if (GameManager.player1won)
+        {
+            winText.text = "Player 1 Won!     Play Again?";
+        }
+        else
+        {
+            winText.text = "Player 2 Won!     Play Again?";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+  
+    }
+}
