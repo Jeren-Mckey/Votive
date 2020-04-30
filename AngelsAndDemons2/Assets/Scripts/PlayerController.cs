53560f96346c9fb2f063b0f09b6b5dc22d35e7f2
Aaron Jay Macaldo <Aaronjay.macaldo@sjsu.edu>
2020-04-29 19:05:28 -0700

    Customized to allow two players, and added onhit functions and parameters

--- AngelsAndDemons2/Assets/Scripts/PlayerController.cs
+++ AngelsAndDemons2/Assets/Scripts/PlayerController.cs
@@ -10,17 +10,33 @@ public class PlayerController : MonoBehaviour
 
     public float playerMovement; //takes speed of movement
     public GameObject playerObject;
+    public bool isPlayerOne; //holds whether this is player 1 or 2 -a
     //public GameObject spawnPoint; //everything related to spawnPoint variable commented out for now
 
+    private GameObject spawnPosition; //holds object that contains spawn location -a
     private bool gravFlipped;
     private float distToGround;
     private int playerJumps;
     private int gravSwitches;
     private float objectHeight;
     private Vector2 screenBounds;
-
+    private int health;
+    private bool hittable = true;
 
     private bool pauseMovement = false; //flag to stop player movement
+
+    // -a
+    void Awake()
+    {
+        /*
+        if (isPlayerOne)
+            spawnPosition = GameObject.Find("P1Spawn");
+        else 
+            spawnPosition = GameObject.Find("P2Spawn");
+
+        this.transform.Translate(spawnPosition.transform.position);
+        */
+    }
 
 
 
@@ -29,7 +45,6 @@ public class PlayerController : MonoBehaviour
     {
         //player movemen animations
         movementAnimator = GetComponent<Animator>();
-
 
         //spawnPoint = GameObject.FindGameObjectWithTag("Spawn");
         gravFlipped = false;
@@ -43,39 +58,73 @@ public class PlayerController : MonoBehaviour
     }
 
     // when player is moving, play animation. 
-    private void Update() 
+    private void Update()
     {
-        float horizontalMove = Input.GetAxisRaw("Horizontal");
+        float horizontalMove;
+        if (isPlayerOne && hittable)
+        {
+            horizontalMove = Input.GetAxisRaw("P1Horizontal");
+            if (horizontalMove == 0)
+            {
+                movementAnimator.SetBool("isRunning", false);
+                movementAnimator.SetBool("isBacking", false);
+            }
+            else if (horizontalMove == -1)
+            {
+                movementAnimator.SetBool("isBacking", true);
+            }
+            else
+            {
+                movementAnimator.SetBool("isRunning", true);
+            }
 
-        if (horizontalMove == 0)
-        {
-            movementAnimator.SetBool("isRunning", false);
-            movementAnimator.SetBool("isBacking", false);
+            if (Input.GetButtonDown("P1Jump"))
+            {
+                movementAnimator.SetTrigger("isJumping");
+            }
+            else if (Input.GetButtonDown("P1Fire1"))
+            {
+                movementAnimator.SetTrigger("isKicking");
+            }
+            else if (Input.GetButtonDown("P1Fire2"))
+            {
+                movementAnimator.SetTrigger("isPunching");
+            }
         }
-        else if (horizontalMove == -1)
+        else if (hittable)
         {
-            movementAnimator.SetBool("isBacking", true);
+            horizontalMove = Input.GetAxisRaw("P2Horizontal");
+            if (horizontalMove == 0)
+            {
+                movementAnimator.SetBool("isRunning", false);
+                movementAnimator.SetBool("isBacking", false);
+            }
+            else if (horizontalMove == -1)
+            {
+                movementAnimator.SetBool("isRunning", true); //-a
+            }
+            else
+            {
+                movementAnimator.SetBool("isBacking", true); //-a
+            }
+
+            if (Input.GetButtonDown("P2Jump"))
+            {
+                movementAnimator.SetTrigger("isJumping");
+            }
+            else if (Input.GetButtonDown("P2Fire1"))
+            {
+                movementAnimator.SetTrigger("isKicking");
+            }
+            else if (Input.GetButtonDown("P2Fire2"))
+            {
+                movementAnimator.SetTrigger("isPunching");
+            }
         }
         else
-        {
-            movementAnimator.SetBool("isRunning", true);
-        }
-
-        if (Input.GetButtonDown("Jump"))
-        {
-            movementAnimator.SetTrigger("isJumping");
-        }
-        else if (Input.GetButtonDown("Fire1"))
-        {
-            movementAnimator.SetTrigger("isKicking");
-        }
-        else if (Input.GetButtonDown("Fire2"))
-        {
-            movementAnimator.SetTrigger("isPunching");
-        }
+            horizontalMove = 0f;
 
         movementAnimator.SetFloat("speed", horizontalMove);
-        
     }
 
     /*
@@ -89,7 +138,7 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        
+
         //Player Camera Interaction
         if (isGrounded())
         {
@@ -114,13 +163,26 @@ public class PlayerController : MonoBehaviour
         }
 
         //Player Movement
-        if (Input.GetKey(KeyCode.RightArrow) && !pauseMovement)
+        if (isPlayerOne && hittable)
         {
-            transform.Translate(Vector3.right * playerMovement * Time.deltaTime);
+            if (Input.GetKey(KeyCode.D) && !pauseMovement)
+            {
+                transform.Translate(Vector3.right * playerMovement * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.A) && !pauseMovement)
+            {
+                transform.Translate(Vector3.left * playerMovement * Time.deltaTime);
+            }
         }
-        if (Input.GetKey(KeyCode.DownArrow) && !pauseMovement)
-        {
-            transform.Translate(Vector3.left * playerMovement * Time.deltaTime);
+        else if(hittable) {
+            if (Input.GetKey(KeyCode.Quote) && !pauseMovement)
+            {
+                transform.Translate(Vector3.right * playerMovement * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.L) && !pauseMovement)
+            {
+                transform.Translate(Vector3.left * playerMovement * Time.deltaTime);
+            }
         }
 
 
@@ -141,11 +203,13 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+
         if (collision.collider.tag == "Floor" || collision.collider.tag == "Object")
         {
             playerJumps = 1;
             gravSwitches = 1;
         }
+        /*
         else if (collision.collider.tag == "Enemy")
         {
             //Reset Gravity
@@ -156,6 +220,7 @@ public class PlayerController : MonoBehaviour
             GameManager.playerHitDelegate -= spawnPlayer;
             Destroy(this.gameObject);
         }
+        */
         else if (collision.collider.tag == "Obstacle")
         {
             //Reset Gravity
@@ -175,6 +240,23 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        PlayerController other = collision.gameObject.GetComponent("PlayerController") as PlayerController;
+        if (collision.collider.tag == "Player" && (movementAnimator.GetCurrentAnimatorStateInfo(0).IsName("punch") || movementAnimator.GetCurrentAnimatorStateInfo(0).IsName("kick"))
+            && movementAnimator.GetCurrentAnimatorStateInfo(0).length > .1 && other.hittable)
+        {
+            other.hittable = false;
+            //Reset Gravity
+            gameObject.GetComponent<Rigidbody2D>().gravityScale = 1;
+
+            //Trigger event
+            
+            other.isHit();
+   
+        }
+    }
+
     void spawnPlayer() //spawnpoint related stuff commented out
     {
         //spawnPoint = GameObject.FindGameObjectWithTag("Spawn");
@@ -190,5 +272,38 @@ public class PlayerController : MonoBehaviour
         if ((-1 * (transform.position.y - objectHeight)) >= screenBounds.y) return true;
         else if ((-1 * (transform.position.y + objectHeight)) >= screenBounds.y) return true;
         else return false;
+    }
+
+    public void isHit()
+    {
+        decHealth myHealth;
+        movementAnimator.SetBool("isFalling", true);
+
+        if(isPlayerOne)
+            myHealth = GameObject.Find("P1HealthBar").GetComponent("decHealth") as decHealth;
+        else
+            myHealth = GameObject.Find("P2HealthBar").GetComponent("decHealth") as decHealth;
+            
+        myHealth.changeLife();
+        StartCoroutine(hitWait());
+    }
+
+    IEnumerator hitWait()
+    {
+        yield return new WaitForSeconds(1);
+        decHealth myHealth;
+        if (isPlayerOne)
+        {
+            myHealth = GameObject.Find("P1HealthBar").GetComponent("decHealth") as decHealth;
+            if (myHealth.startingLife == 0)
+                GameManager.loadWinner(false);
+        }
+        else {
+            myHealth = GameObject.Find("P2HealthBar").GetComponent("decHealth") as decHealth;
+            if (myHealth.startingLife == 0)
+                GameManager.loadWinner(true);
+        }
+
+        hittable = true;
     }
 }
